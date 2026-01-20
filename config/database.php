581b0e9e7b9f1ce34021c484f735f1e2f9804f95
Alexandre592 <xano301@gmail.com>
2026-01-20 10:26:09 +0100

    Add files via upload

--- config/database.php
+++ config/database.php
@@ -17,7 +17,6 @@ $options = [
 // Connexion PDO
 try {
      $pdo = new PDO($dsn, $username, $password, $options); 
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
+} 
+
 ?>
